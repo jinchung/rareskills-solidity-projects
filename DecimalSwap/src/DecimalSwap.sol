c4e2cac5ec2948eb45233b2fd64bf5383e8a6e18
jinchung <jin.chung17@gmail.com>
2026-04-16 16:20:12 -0400

    DecimalSwap

--- DecimalSwap/src/DecimalSwap.sol
+++ DecimalSwap/src/DecimalSwap.sol
@@ -15,11 +15,29 @@ contract DecimalSwap {
     IERC20Metadata public immutable tokenB;
 
     constructor(address tokenA_, address tokenB_) {
+      tokenA = IERC20Metadata(tokenA_);
+      tokenB = IERC20Metadata(tokenB_);
     }
 
     function swapAtoB(uint256 amountIn) external {
+      swapTokenToToken(tokenA, tokenB, amountIn);
     }
 
     function swapBtoA(uint256 amountIn) external {
+      swapTokenToToken(tokenB, tokenA, amountIn);
+    }
+
+    function swapTokenToToken(IERC20Metadata tokenIn, IERC20Metadata tokenOut, uint256 amountIn) private {
+      // convert tokenIn amountIn to tokenOut amountOut
+      uint256 tokenOutDecimals = uint256(tokenOut.decimals());
+      uint256 tokenInDecimals = uint256(tokenIn.decimals());
+      uint256 amountOut = (amountIn * 10**tokenOutDecimals) / (10**tokenInDecimals);
+      address me = address(this);
+
+      // transfer tokenIn from the user
+      tokenIn.safeTransferFrom(msg.sender, me, amountIn);
+
+      // transfer tokenOut to the user
+      tokenOut.transfer(msg.sender, amountOut);
     }
 }
