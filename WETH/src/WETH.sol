fd5ed90f5b76ff1b9539e1550f7f3821416e0f7d
jinchung <jin.chung17@gmail.com>
2026-04-19 20:58:46 -0400

    WETH

--- WETH/src/WETH.sol
+++ WETH/src/WETH.sol
@@ -7,8 +7,10 @@ contract WETH is ERC20("Wrapped Ether", "WETH") {
 
 
     function deposit() external payable {
+      _mint(msg.sender, msg.value);
     }
 
     function withdraw(uint256 amount) external {
+      _burn(msg.sender, amount);
     }
 }
